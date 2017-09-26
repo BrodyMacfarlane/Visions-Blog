ab91d7b7b387910510e40a4822957d01e9e5ae38
Brody Macfarlane <macfarlanebrody@gmail.com>
2017-09-25 20:11:29 -0600

    learnin mo.js and lovin it

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,16 +1,55 @@
-const yCurve = new MojsCurveEditor({ name: 'yOffset' });
-const xCurve = new MojsCurveEditor();
+// const yCurve = new MojsCurveEditor({ name: 'yOffset' });
+// const xCurve = new MojsCurveEditor();
+// yCurve.getEasing()
+
+var navBar = new mojs.Shape({
+	repeat: 5,
+	isYoyo: true,
+	width: '100%',
+	height: 65,
+	shape: 'rect',
+	fill: { '#FFD6D1' : '#D1F4FF' },
+	isShowStart:  true,
+	className: 'yoyoyo',
+	duration: 1200
+}).then({
+	fill: { '#D1F4FF' : '#FFD6D1' }
+}).then({
+	fill: '#FFF'
+}).play()
 
 var mainTriangle = new mojs.Shape({
+	width:        400,
+	height:       400,
 	shape:        'polygon',
-	radius:       200,
-	radiusY:      200, // explicit radiusY
-	left:         '50%',
-	top: 		  '50%',
-	fill:         { 'rgb(0,0,0,0)' : '#1E1E1E', curve: yCurve.getEasing() },
-	isShowStart:  true,
-	duration:     2000,
-	angle:		  { '0' : '60', curve: xCurve.getEasing() }
-})
+	radiusX:      200,
+	radiusY:      200,
+	fill:         'none',
+	stroke:           { '#FFD6D1' : '#D1F4FF' },
+	strokeWidth:      '10',
+  	strokeDasharray:  '100%',
+  	strokeDashoffset: { '-100%' : '100%' },
+ 	y:               -20,
+	isShowStart:  false,
+	duration:     1200,
+	angle:		  180
+}).then({
+	radiusX:      0,
+	duration:     0
+}).then({
+	radiusX:      200,
+	stroke:       'none',
+	fill:         { '#D1F4FF' : '#FFD6D1' },
+	duration:     1200
+}).then({
+	fill:         '#222222'
+}).play()
 
-new MojsPlayer({ add: mainTriangle });
+const visionsText = new mojs.Html({
+	el: '#visions-text',
+	delay: 1400,
+	opacity: {'0':'1'},
+	duration: 1000
+}).play()
+
+// new MojsPlayer({ add: mainTriangle, navBar });
